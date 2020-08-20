8037929d3e27e7e70dffa0c472f62da19ddff451
jazmin99sanchez <jazmin98snchz@outlook.es>
2020-08-19 22:13:07 -0500

    facturas.js basico

---
+++ pantallas/Facturas.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export class Facturas extends Component {
+    render(){
+        return <View>
+        <Text>Facturas</Text>
+        </View>
+    }
+}
